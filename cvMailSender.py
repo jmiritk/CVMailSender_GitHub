04281f9394c452f35f56042416f212c20131b951
jmiritk <jmiritk@gmail.com>
2016-10-19 00:59:09 +0300

    documentation added

--- cvMailSender.py
+++ cvMailSender.py
@@ -5,6 +5,9 @@ import email.mime.application
 import yaml
 import sys
 
+'''This is a program that receives an email address and auto send email with CV file both in pdf and word formats.
+    The input argument is a path to yaml configuration file, where all mail settings are written.'''
+
 
 def main():
     receiver = getReceiverEmail()
@@ -15,23 +18,16 @@ def main():
     sendMail(config["mail_auth"]['email'], receiver, config["mail_auth"]['password'], msg)
     main()
 
+#user input - mail address to send mail
 def getReceiverEmail():
     return str(raw_input('Pleaser Enter Receiver ')).lower().strip()
 
-
-def attachFile(filePath, filename, fileType, msg):
-    with open(filePath, 'rb') as fp:
-        att = email.mime.application.MIMEApplication(fp.read(), _subtype=fileType)
-        fp.close()
-        att.add_header('Content-Disposition', 'attachment', filename=filename)
-        msg.attach(att)
-        print 'finished reading' + fileType
-        return msg
-
+#take argument from user as the path to find yaml configuration file
 def readConfig():
     with open(sys.argv.pop(1), 'r') as f:
         config = yaml.load(f)
         return config
+
 
 def createMail(receiver, config):
     msg = email.mime.Multipart.MIMEMultipart()
@@ -43,6 +39,16 @@ def createMail(receiver, config):
     msg.attach(body)
     print 'attached mail body '
     return msg
+
+#generic function to attach file of some type to the mail
+def attachFile(filePath, filename, fileType, msg):
+    with open(filePath, 'rb') as fp:
+        att = email.mime.application.MIMEApplication(fp.read(), _subtype=fileType)
+        fp.close()
+        att.add_header('Content-Disposition', 'attachment', filename=filename)
+        msg.attach(att)
+        print 'finished reading' + fileType
+        return msg
 
 def sendMail(sender, receiver, password, msg):
     server = smtplib.SMTP('smtp.gmail.com:587')
